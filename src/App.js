f8055c70d363799569e2da7b7779afc703ad652b
rozie321 <107440478+rozie321@users.noreply.github.com>
2025-05-02 10:42:17 +0100

    bootstrap instal and initial HTML

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 
 import './App.css';
+import React from 'react';
+import Weather from './Weather';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Weather APP</h1>
+        <h1>Weather APP</h1>
+      </header>
+      <Weather />
+      
+       
+         <p>
+          This is a simple weather app that allows you to search for the current weather in any city. 
+          It uses the OpenWeatherMap API to fetch the weather data and displays it in a user-friendly format.
+        </p>
+     
+
       <footer>
         <p>Created by Rozie Manesh source  <a href="https://github.com/rozie321/reactweatherapp"> github </a>
          and  <a href="https://reactweatherapp-roselyn.netlify.app/" > 
            netlify </a>
         </p>
         </footer>
-      <p>
-        This is a simple weather app that allows you to search for the current weather in any city. 
-        It uses the OpenWeatherMap API to fetch the weather data and displays it in a user-friendly format.
-      </p>
-     
-      </header>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import  './Weather.css';
+
+
+export default function Weather() {
+    return(
+        <div className ="Weather">
+        <h1>Lisbon</h1>
+        <ul>
+            <li>Wednesday: 0700</li>
+            <li>Mostly Cloudy</li>
+            
+        </ul>
+
+        </div>
+    )
+}
