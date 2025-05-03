095701b937415be6f0bea5e5aa6063455e972faf
rozie321 <107440478+rozie321@users.noreply.github.com>
2025-05-04 00:14:07 +0100

    CSS styling

--- src/App.js
+++ src/App.js
@@ -2,30 +2,22 @@
 import './App.css';
 import React from 'react';
 import Weather from './Weather';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import  './Weather.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather APP</h1>
-      </header>
-      <Weather />
       
-       
-         <p>
-          This is a simple weather app that allows you to search for the current weather in any city. 
-          It uses the OpenWeatherMap API to fetch the weather data and displays it in a user-friendly format.
-        </p>
-     
-
-      <footer>
-        <p>Created by Rozie Manesh source  <a href="https://github.com/rozie321/reactweatherapp"> github </a>
-         and  <a href="https://reactweatherapp-roselyn.netlify.app/" > 
-           netlify </a>
-        </p>
-        </footer>
-    </div>
+        <Weather />
+        <footer>
+          <p>Created by Rozie Manesh source  <a href="https://github.com/rozie321/reactweatherapp"> github </a>
+          and  <a href="https://reactweatherapp-roselyn.netlify.app/" > 
+            netlify </a>
+          </p>
+          </footer>
+      </div>
+      
   );
 }
 
--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,50 @@
 import React from "react";
 import  './Weather.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Weather() {
     return(
         <div className ="Weather">
-        <h1>Lisbon</h1>
-        <ul>
-            <li>Wednesday: 0700</li>
-            <li>Mostly Cloudy</li>
-            
-        </ul>
+            <form>
+                <div className="row"> 
 
+                <div className="col-9">
+
+                    <input type="text" placeholder="Enter a city"
+                    className="form-control" id="city-input" />
+                </div>
+                <div className="col-3">
+                    <button type="submit" className="btn btn-primary">
+                        Search
+                    </button>
+                </div>
+                </div>
+            </form>
+            <h1>Lisbon</h1>
+            <ul>
+                <li>Wednesday: 0700</li>
+                <li>Mostly Cloudy</li>
+                
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img 
+                    src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Weather Icon" />
+                   <span className="temperature">
+                    6
+                    </span>  
+                    <span className="unit">°C|°F</span>   
+
+                </div>
+                <div className="col-6">
+                    <ul>
+                            <li>Precipitation: 15%</li>
+                            <li>Humidity: 60%</li>
+                            <li>Wind: 10 km/h</li>
+                        </ul>
+                </div>
+            </div>
         </div>
     )
 }
